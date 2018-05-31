7e323a7c06288e310716aab1c8c8224e09a7ac69
taculin <34300696+taculin@users.noreply.github.com>
2018-05-31 16:50:34 +0800

    Update clicker.py
    This is updated May 24 2018 at Marinduque.
    
    vp2All(fname, fps) : extracts fps frames from videos
       input:
              fname is full. Ex: "video12.avi" or "file.mp4"
              fps: if not indicated, default is 4 frames per second
       output:
              frames: fname_r<fps:2>_n<i:5>.jpg
    
    main(fname, last_i): loops over frames
             fname: fname_r<fps:2>

--- clicker.py
+++ clicker.py
@@ -7,24 +7,34 @@ Created on Fri May 11 10:14:53 2018
 
 import cv2
 
-FPS = 8
-
+    
+#filename is full (include extension)
+def v2pALL(fnameWithExtn, fps=4):
+    cap = cv2.VideoCapture(fnameWithExtn)
+    last, frate =  int(cap.get(cv2.CAP_PROP_FRAME_COUNT)), int(cap.get(cv2.CAP_PROP_FPS))
+    k=frate/fps 
+    i,j=0,0
+    print frate,k,'extracting', (last/k), 'items (out of', last,')'
+    while(cap.isOpened()) and i<last and i<10:
+        ret, frame = cap.read()
+        if ret==True and (j%k)==0:
+            cv2.imwrite(fnameWithExtn.split('.')[0]+"_r{0:0>2}".format(fps)+"_n{0:0>5}".format(i)+'.jpg',frame)    
+            i=i+1
+            print '.',
+        j=j+1
+    cap.release()
+    cv2.destroyAllWindows()
 
 def col(i):
     C = [(0,255,0),(102,102,255),(255,102,255),(255,255,102),(102,102,0),(153,204,255),(153,0,153),(255,102,0)]
     return C[i%len(C)]
 
 import Tkinter, tkFileDialog, tkMessageBox
-def openf():    
-    Tkinter.Tk().withdraw()
-    path = tkFileDialog.askopenfilename()
-    fname = path.split('/').pop().encode('ascii','ignore')
-    return fname
 # read the existing NTXY records
 def f2l(fname):
     lst = []
     try:    
-        with open(fname.split('.')[0]+'.dat','r') as file:
+        with open(fname+'.dat','r') as file:
             for line in file:
                 row = [int(c) for c in line.split(' ')]
                 lst.append(row)
@@ -32,22 +42,10 @@ def f2l(fname):
     except IOError:
         print 'No data file found'
     return lst    
-def readf():
-    f = openf()
-    if f:
-        cap = cv2.VideoCapture(f)
-        return cap, f2l(f), f
-    else:
-        return None, None, None
-
-#fc,fh,fw,fps
-def capStat(cap):
-    return int(cap.get(cv2.CAP_PROP_FRAME_COUNT)),int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)),int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),int(cap.get(cv2.CAP_PROP_FPS))
-
 
 # overwrite the NTXY records    
 def l2f(R,fname):
-    with open(fname.split('.')[0]+'.dat','w') as file:
+    with open(fname+'.dat','w') as file:
         for row in R:
             if len(row)<5:
                 file.write(' '.join(map(str, row))+"\n")
@@ -56,43 +54,42 @@ def l2f(R,fname):
 
 def lop(f,R,I,i,n):
     key = cv2.waitKey(0)
-    # inc/dec car number
-    if key == ord('+'):
+  
+    if key == ord('+'):     # inc car number
         n=n+1
-    elif key == ord('-'):
+    elif key == ord('-'):   # dec car number
         if n>=0:
             n=n-1
-    # delete most recent coord (pop stack)
-    elif key == ord('d'):
+    elif key == ord('d'):   # delete most recent coord (pop stack)
         print 'removed recent item ',R.pop()
     # overwrite data to file
     elif key == ord('s'):
         l2f(R,f)       
-    # navigate frame: next/back commands 
-    elif key == ord('l'):
+    
+    elif key == ord('l'):   # next frame
         i=i+1
         if i>=I:
             i=0
-    elif key == ord('r'):
+    elif key == ord('r'):   # previous frame 
         i=i-1
         if i<0:
             i=I-1
-    elif key == ord(' '):
+    elif key == ord(' '):   # go to frame previously clicked 
         n,i,x,y =  R[-1]
-    elif key == ord('q') or key == 27:
+    elif key == ord('q') or key == 27: # quit q or esc
+        root = Tkinter.Tk()
+        root.withdraw()
         if len(R)> 0 and (tkMessageBox.askquestion("Closing window", "You want to save the coordinates first?", icon='warning') == 'yes'):
             l2f(R,f)
+            print 'coordinates saved at',f
+        else:
+            print len(R),'coordinates discarded'
+        root.destroy()    
     return key,i,n
 
-def getImg(cap,i):    
-    t = (i/FPS)*1000+((i%FPS)*(1000.0/FPS))
-    try:
-        cap.set(cv2.CAP_PROP_POS_MSEC,t)
-        print 'fetching image at',cap.get(cv2.CAP_PROP_POS_MSEC)
-        ret, I = cap.read()
-        return I
-    except:
-        return None
+#we need to modularize this to catch input errors
+def getImg(fname,i):   
+    return cv2.imread(fname+"_n{0:0>5}".format(i)+'.jpg')
     
 def shoImg(f,img,N,i,R):
     path = [r for r in R if r[1]<=i and r[1]>=i-10]
@@ -103,51 +100,41 @@ def shoImg(f,img,N,i,R):
     currCarClicked = [n for n in currClicked if n==N]
     h,w = img.shape[:2]
     c = col(N)
-    s = .4    
+    s = .6    
     frame = '[f:'+str(i)+','+str(len(currClicked))+']'
     count = '[c:'+str(N)+','+str(len(currCarClicked))+']'
-    cv2.putText(img,frame,(5,5),cv2.FONT_HERSHEY_SIMPLEX,s,c)
-    cv2.putText(img,count,(60,5),cv2.FONT_HERSHEY_SIMPLEX,s,c)
+    cv2.putText(img,frame,(20,40),cv2.FONT_HERSHEY_SIMPLEX,s,c)
+    cv2.putText(img,count,(100,40),cv2.FONT_HERSHEY_SIMPLEX,s,c)
     # show
     cv2.imshow(f,img)
-
-     
-
-def main():   
-    cap,R,f = readf()
-    if not cap:
-        return
+    
+def main(f,L):   
+    R=f2l(f)
     i,N = 0,0
-    img=getImg(cap,i)
-    shoImg(f,img,N,i,R)
+    img=getImg(f,i)
+    shoImg(f,img.copy(),N,i,R)
 
     
     def onMseClk(event,x,y,flags,param):
         if event == cv2.EVENT_LBUTTONDOWN:
-            img=getImg(cap,i)
+            im = img.copy()
             R.append([N,i,x,y])
-            shoImg(f,img,N,i,R)
+            shoImg(f,im,N,i,R)
             print 'added (',x,',',y,') for car ',N,' at frame ',i
         if event == cv2.EVENT_MOUSEMOVE:
-            img=getImg(cap,i)
+            im = img.copy()
             h,w=img.shape[:2]
-            cv2.putText(img,str(x)+','+str(y),(w-200,40),cv2.FONT_HERSHEY_SIMPLEX,0.8,(255,255,255))
-            shoImg(f,img,N,i,R)
+            cv2.putText(im,str(x)+','+str(y),(w-200,40),cv2.FONT_HERSHEY_SIMPLEX,0.8,(255,255,255))
+            shoImg(f,im,N,i,R)
             
     cv2.setMouseCallback(f, onMseClk)
-    
-    
-    fcs,_,_,fps = capStat(cap)
-    L = fcs/fps*FPS
-    print 'len:',L,' frcount:',fcs,'rate:',fps
     key=0
     while not key==ord('q'):
-        img=getImg(cap,i)
-        shoImg(f,img,N,i,R)
+        img=getImg(f,i)
+        shoImg(f,img.copy(),N,i,R)
         key,i,N = lop(f,R,L,i,N)
         if key in [ord('l'),ord('r')]:
-            print i,cap.get(cv2.CAP_PROP_POS_MSEC)
+            print i
 
-    cap.release()
     cv2.destroyAllWindows()
             
