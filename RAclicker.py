2ccd100d580615970ef9353af19dfdabd66d995c
taculin <34300696+taculin@users.noreply.github.com>
2018-09-16 17:29:17 +0800

    updated the decom method
    removing last variable on both methods

--- RAclicker.py
+++ RAclicker.py
@@ -13,8 +13,8 @@ def decom(fname):
     fn = fname.split('.')[0]
     i=0
     cap = cv2.VideoCapture(fn+'.mp4')
-    last = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    while(cap.isOpened()) and i<last:
+#    last = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    while cap.isOpened(): #and i<last:
         ret, frame = cap.read()        
         if ret==True:
             frame = cv2.resize(frame, None, fx=0.75, fy=0.75)
